e901d20aeb94ba709d6d3eb659c09cbd8c4ed158
villamont <villamont1578@gmail.com>
2023-08-11 23:30:58 -0600

    version final tarea 2

---
+++ Tarea_2/Documentacion_tarea2.md
@@ -0,0 +1,84 @@
+# Tarea 1: API de la NASA 
+##  APIs de la [NASA](https://api.nasa.gov/?ref=apispublicas.com#CAD) consumidas
+- Asteroids NeoWS
+- Mars Rovers Photos
+
+## Asteroids NeoWS
+NeoWs (Near Earth Object Web Service) por sus siglas en Ingles, es el servicio web de detección de objetos cercanos a la Tierra enfocada en asteroides.
+Funcionalidades de NeoWs:
+- Buscar Asteroides basado en la fecha de mayor cercanía a la tierra.
+- Buscar un Asteroide específico usando el id asignado por el equipo JPL de la NASA obteniendo información generalizada del objeto.
+
+Toda la información es tomada del proyecto de Asteriodes del equipo JPL de la NASA. [NASA JPL](http://neo.jpl.nasa.gov/)
+
+## Mars Rover Photos
+Esta API es diseñada para recopilar información de imagenes tomadas por los equipos Curiosity, Opportunity y Spirit del proyecto Rovers en Marte y hacerla más fácilmente accesible para otros desarrolladores, educadores y la comunidad científica en general.
+Cada Rover tiene su propio grupo de fotos almacenado en la base de datos, las cuales pueden ser accesadas independientemente. Las fotos están organizadas por la fecha Marciana en que fueron tomadas comenzando desde el día de llegada del Rover a Marte el cual sería el día 0.
+Las fotos también se pueden accessar por la fecha Terrestre correspondiente.
+
+## Extrayendo fotos por fecha Marciana(sol)
+El dato sol se determina por el día de llegada del Rover a Marte siendo el primer día sol=0 y el último el día mas reciente del que se tengan fotografías 
+- Ejemplo de endpoint con fecha sol=1000 (1001 día 1001 del Rover en Marte): 
+https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY
+
+## Extrayendo fotos por fecha terrestre
+Se puede también extraer fotos por fecha Terrestre en formato YYYY-MM-DD (Año, mes, día).
+- Ejemplo de endpoint con fecha Terrestre de 2015-06-03: 
+https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-06-03&api_key=DEMO_KEY
+
+DEMO_KEY es una llave para extraer datos de las APIs con ciertas limitaciones en la cantidad de solicitudes que se le pueden hacer al servidor, para usos extensos de datos es necesario solicitar una KEY personal en el sitio web de la [NASA](https://api.nasa.gov/?ref=apispublicas.com#CAD)
+
+
+## Datos recopilados en el proyecto
+### *Asteroids NeoWs*
+- Cantidad de total de Asteroides cercanos a la tierra en un rango de fechas Terrestres
+
+### *Mars Rovers*
+- Fotos totales tomadas por el Rover Curiosity y Opportunity en un día en específico con fecha terreste.
+
+## Funciones implementadas en la tarea
+### getAsteroids():
+Esta función hace una solicitud a la API Asteroids NeoWs, recibe 2 argumentos(fecha inicial y fecha final) que corresponden a un rango de fechas terrestres en formato (YYYY-MM-DD) (Año, mes, día) en el cual se buscara la cantidad de asteroides cercanos a la Tierra.
+El rango de fechas debe cumplir las siguientes condiciones:
+- Los datos de Asteroides cercanos a la tierra esta disponible desde 1900-01-01 a la fecha actual (2023-07-04)
+- El rango de fechas a escoger debe ser máximo de 5 días
+### Endpoint
+- https://api.nasa.gov/neo/rest/v1/feed?start_date=variable_star_date&end_date=variabe_end_date&api_key=T21hmrUCC7Y5kIsbbI1wZtYvgiJo60mx5QERfPBd
+- Ejemplo: https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-09&api_key=T21hmrUCC7Y5kIsbbI1wZtYvgiJo60mx5QERfPBd
+
+El servidor devuelve un Diccionario anidado en el cual el valor asignado a cada llave es del tipo lista, existen diferentes niveles de anidado segun la información disponible en el servidor.
+La respuesta del servidor contiene la siguiente informacion de los asteroides detectados dentro del rango de fechas proporcionado por el usuario.
+- id del Asteroide
+- Diámetro del Asteroide em KM, Millas, pies
+- Distancia mas cercana a la tierra dentro del rango de fechas dado por el usuario
+- Velocidad del Asteroide entre otros muchos mas.
+
+La función getAsteroids() le muestra al usuario la cantidad total de asteroides detectados en cada uno de los dias del rango de fechas proporcionado por el usuario.
+Asi como tambien un grafico de barras representando la cantidad de fotografias tomadas cada uno de los dias del rango seleccionado.
+
+
+### getOpportunity():
+Esta función hace una solicitud a la API MARS ROVER PHOTOS, específicamente a la base de datos de la sonda Opportunity y devuelve la cantidad de fotos tomadas por las diferentes camaras instaladas en dicha sonda en un día determinado por el usuario.
+La base de datos contiene información de las fotografías tomadas entre las fechas de 2004-01-26 hasta 2018-06-07
+### Endppoint
+- https://api.nasa.gov/mars-photos/api/v1/rovers/Opportunity/photos?earth_date=Variable_fecha&api_key=T21hmrUCC7Y5kIsbbI1wZtYvgiJo60mx5QERfPBd
+- Ejemplo: https://api.nasa.gov/mars-photos/api/v1/rovers/Opportunity/photos?earth_date=2021-02-26&api_key=T21hmrUCC7Y5kIsbbI1wZtYvgiJo60mx5QERfPBd
+
+Esta función recibe como argumentos (Año y mes) en el formato (YYYY-MM)  y devuelve la cantidad total de fotos tomadas por las camaras de la sonda Opportunity cada dia del mes del año escojido por el usuario.
+Asi como tambien realiza una grafica de lineas de la cantidad de fotografias tomadas por la Sonda Opportunity cada uno de los dias del mes escojido por el usuario.
+
+### getCuriosity():
+Esta función hace una solicitud a la API MARS ROVER PHOTOS, específicamente a la base de datos de la sonda Curiosity y devuelve la cantidad de fotos tomadas por las diferentes camaras instaladas en dicha sonda en un día determinado por el usuario.
+La base de datos contiene información de las fotografías tomadas entre las fechas de 2014-10-01 a la fecha actual (2023-08-04)
+### Endpoint
+- https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=Variable_fecha&api_key=T21hmrUCC7Y5kIsbbI1wZtYvgiJo60mx5QERfPBd
+- Ejemplo: https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-02-26&api_key=T21hmrUCC7Y5kIsbbI1wZtYvgiJo60mx5QERfPBd
+
+
+Esta función recibe como argumentos (Año y mes) en el formato (YYYY-MM)  y devuelve la cantidad total de fotos tomadas por las camaras de la sonda Curiosity cada dia del mes del año escojido por el usuario.
+Asi como tambien realiza una grafica de lineas de la cantidad de fotografias tomadas por la Sonda Curiosity cada uno de los dias del mes escojido por el usuario.
+
+
+### NOTA: Algunos de los rangos de tiempo pueden dar error debido a la falta de informacion disponible en la base de datos, en algunos casos se da por perdida de comunicacion de la Sonda con las sistemas Terrestres lo cual no permite la captura de los datos de la Sonda durante el periodo de falla.
+
+### Tambien el programa puede dar errores para años biciestos si se escoge el mes de Febrero
--- Tarea_2/plotting_API_NASA.py
+++ Tarea_2/plotting_API_NASA.py
@@ -40,36 +40,37 @@ def getAsteroids(fecha_inicial, fecha_final):
     if getResponse.status_code == 200:
         data = getResponse.json()
         aste=data["near_earth_objects"]
-        #print(data)
-        #print("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n")
-        #print("Cantidad de asteroides cercanos de la tierra para el rango de fechas :",start_date, "al", end_date, "es: ", data["element_count"])
-        print("\nDeberia de haber 4 elementos en aste:\n", len(aste), 'aste es del tipo', type(aste))
         
-        #creando lista de asteroides por dia
+            #creando lista de asteroides por dia
         asters_dia=[]
         for llave in aste:
-            print("\n\n tamanio de llave es: ",len(aste[llave]))
             asters_dia.append(len(aste[llave]))
             prueba=aste[llave]
-        
+
+
         ##creando lista de llaves 
         list_llaves=aste.keys()
         list_keys=list(list_llaves)
 
-        print("\nImprimiendo lista de llaves \n",list_keys, 'que es de tipo', type(list_keys))
-        print("\n Imprimiendo asters_dia", asters_dia)
-
         ##concatenar 2 listas para crear el Dataframe
-        array=pd.DataFrame({'Fecha': 
-                        list_keys,
-                      'Cantidad de asteriodes': 
-                      asters_dia,
-                        })
+        #array=pd.DataFrame({'Fecha': 
+        #                list_keys,
+        #              'Cantidad de asteriodes': 
+        #              asters_dia,
+        #                })
         print("----------------------------------------------------------------------------------------------------------------------------\n\n")       
-        plt.plot(array['Fecha'], array['Cantidad de asteriodes'])
+        
+        plt.bar(list_keys, asters_dia)
+        plt.title('Asteroides Cercanos a las tierra')
+        ax=plt.subplot()
+        ax.set_xlabel("Fecha")
+        ax.set_ylabel("Cantidad de Asteriodes")
         plt.show()
+        
     else:
         print('\nError al realizar la solicitud, por favor escoje otro rango diferente de fechas')
+    
+    return
 
 ##-------------------------Funcion para solicitar cantidad de photos tomadas por Opportunity---------------------------------
 
@@ -79,11 +80,11 @@ def getOpportunity(anio, mes, month_num):
     year=anio
     month=str(month_num)
     key='&api_key=T21hmrUCC7Y5kIsbbI1wZtYvgiJo60mx5QERfPBd'
+    fotos_por_dia=[]
     
     ##iterando sobre la cantidad de dias que tiene el mes escogido por el usuario
     cantidad_dias=meses[mes]
     numero_dias=int(cantidad_dias)
-    #print("Cantidad de dias es: ", numero_dias, "del mes", mes)
     print('\n\nExtrayendo informacion de la base de datos de Opportunity\n ...')
 
     dias_mes=list()
@@ -102,41 +103,37 @@ def getOpportunity(anio, mes, month_num):
            print('\n====> Error al realizar la solicitud, por favor ingresa una fecha valida')
 
     ##concatenar 2 listas para crear el Dataframe
-    array=pd.DataFrame({'Fecha': 
-                        dias_mes,
-                      'Cantidad de fotos': 
-                      fotos_por_dia,
-                        })
+    #array=pd.DataFrame({'Fecha': 
+    #                    dias_mes,
+    #                  'Cantidad de fotos': 
+    #                  fotos_por_dia,
+    #                    })
     
-    print("Imprimiendo cantidad de fotos tomadas por dia: \n", array)
+    #print("Imprimiendo cantidad de fotos tomadas por dia: \n", array)
     
     ##extrayendo el dia en que se tomaron mas fotos y la fecha respectiva
-    x=array['Cantidad de fotos'].max()
-    maxs=array['Cantidad de fotos'].idxmax()
+    #x=array['Cantidad de fotos'].max()
+    #maxs=array['Cantidad de fotos'].idxmax()
     
-    print('\nEl dia', dias_mes[maxs], "se tomo la mayor cantidad de fotos, un total de:  ", x, "\n")
+    #print('\nEl dia', dias_mes[maxs], "se tomo la mayor cantidad de fotos, un total de:  ", x, "\n")
     
     
 
     ##Realizando la graficacion de los datos
     print("\nGraficando cantidad de fotos tomadas por la Sonda Opportunity por dia para el mes: ", mes, 'del año', anio )
     plt.plot(dias_mes, fotos_por_dia, color='b', linestyle='solid', marker='o', label='Cantidad de fotos por dia') 
-    plt.ylabel('Cantidad de fotos por dia')
-    plt.title('Fotos Mes ')  
+    plt.ylabel('Cantidad de fotos')
+    plt.title('Fotos Tomadas por Opportunity')  
     plt.xticks(rotation=45, fontsize=6)
     plt.legend()
     plt.show()
 
+    return
 
-    
-
-
-
-
-    
+     
 
 ##-------------------------Funcion para solicitar cantidad de photos tomadas por curiosity----------------------------------
-#url_curiosity="https://api.nasa.gov/mars-photos/api/v1/manifests/Curiosity?api_key=T21hmrUCC7Y5kIsbbI1wZtYvgiJo60mx5QERfPBd&sol=800"
+
 
 
 def getCuriosity(anio, mes, month_num):
@@ -144,56 +141,58 @@ def getCuriosity(anio, mes, month_num):
     year=anio
     month=str(month_num)
     api_key='&api_key=T21hmrUCC7Y5kIsbbI1wZtYvgiJo60mx5QERfPBd'
+    fotos_x_dia=[]
     
 ##iterando sobre la cantidad de dias que tiene el mes escogido por el usuario
     cantidad_dias=meses[mes]
     numero_dias=int(cantidad_dias)
+    
+
     #print("Cantidad de dias es: ", numero_dias, "del mes", mes)
     print('\n\nExtrayendo informacion de la base de datos de Curiosity\n ...')
 
-    dias_mes=list()
+    dias_month=list()
     for dia in range(1, numero_dias+1):
         earth_date=anio + '-' + month + '-' + str(dia)
-        dias_mes.append(earth_date)
+        dias_month.append(earth_date)
         url_curiosity= p1 + earth_date + api_key
              
         getResponse = requests.get(url_curiosity)
                 
         if getResponse.status_code == 200:
             fot_Curiosity = getResponse.json()
-            fotos_por_dia.append(len(fot_Curiosity["photos"]))
-            #print("\n\n\nXXXXXXXXXXXXXXX--Mostrando cantidad de fotos tomadas por Mars Curiosity segun fecha dada por el usuario--XXXXXXXXXXXXXXX")
-            #print("Cantidad total de fotos tomadas por Curiosity:", fot_Curiosity["photos"][0]["rover"]["total_photos"])
-            #print('Cantidad total de fotos tomadas por Curiosity el dia:',earth_date, " fue ", len(fot_Curiosity["photos"]) )
-            #print("----------------------------------------------------------#---------------------------------------------------------------------\n\n")
+            fotos_x_dia.append(len(fot_Curiosity["photos"]))
+            
         else:
            print('\n====> Error al realizar la solicitud, por favor ingresa una fecha valida')
 
     ##concatenar 2 listas para crear el Dataframe
-    array=pd.DataFrame({'Fecha': 
-                        dias_mes,
-                      'Cantidad de fotos': 
-                      fotos_por_dia,
-                        })
+    #array=pd.DataFrame({'Fecha': 
+    #                    dias_mes,
+    #                  'Cantidad de fotos': 
+    #                  fotos_por_dia,
+    #                    })
     
-    print("Imprimiendo cantidad de fotos tomadas por dia: \n", array)
+    #print("Imprimiendo cantidad de fotos tomadas por dia: \n", array)
     
     ##extrayendo el dia en que se tomaron mas fotos y la fecha respectiva
-    x=array['Cantidad de fotos'].max()
-    maxs=array['Cantidad de fotos'].idxmax()
+    #x=array['Cantidad de fotos'].max()
+    #maxs=array['Cantidad de fotos'].idxmax()
     
-    print('\nEl dia', dias_mes[maxs], "se tomo la mayor cantidad de fotos, un total de:  ", x, "\n")
+    #print('\nEl dia', dias_mes[maxs], "se tomo la mayor cantidad de fotos, un total de:  ", x, "\n")
     
     
 
     ##Realizando la graficacion de los datos
     print("\nGraficando cantidad de fotos tomadas por la Sonda Curiosity por dia para el mes: ", mes, 'del año', anio )
-    plt.plot(dias_mes, fotos_por_dia, color='b', linestyle='solid', marker='o', label='Cantidad de fotos por dia') 
-    plt.ylabel('Cantidad de fotos por dia')
-    plt.title('Fotos Mes ')  
+    plt.plot(dias_month, fotos_x_dia, color='b', linestyle='solid', marker='o', label='Cantidad de fotos por dia') 
+    plt.ylabel('Cantidad de fotossi')
+    plt.title('Fotos Tomadas por Curiosity ')  
     plt.xticks(rotation=45, fontsize=6)
     plt.legend()
     plt.show()
+
+    return
 
 
     
